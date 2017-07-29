97b370dd42474c050ce018f8ca7855e591ec8ad7
Sally Young <sally@justafish.co.uk>
2017-07-30 00:55:12 +0100

    Styling updates

--- src/components/02_molecule/Teaser/Teaser.jsx
+++ src/components/02_molecule/Teaser/Teaser.jsx
@@ -3,8 +3,14 @@ import Picture from '../../01_atom/Picture/Picture';
 import { StyleSheet, css } from 'aphrodite';
 
 const Teaser = (props) => (
-  <div>
-    <div className={css(styles.header)}>
+  <div className={css(styles.container)}>
+    {!props.flipped &&
+      <div className={css(styles.picture)} >
+        <Picture uri={props.image} />
+      </div>
+    }
+
+    <div className={css(styles.text)}>
       {props.surtitle &&
         <div>
           {props.surtitle}
@@ -13,24 +19,34 @@ const Teaser = (props) => (
       <div>
         {props.title}
       </div>
-      <div>
+      <div className={css(styles.subtitle)}>
         {props.subtitle}
       </div>
     </div>
-    <div className={css(styles.picture)} >
-      <Picture uri={props.image} />
-    </div>
+
+    {props.flipped &&
+      <div className={css(styles.picture)} >
+        <Picture uri={props.image} />
+      </div>
+    }
   </div>
 );
 
 const styles = StyleSheet.create({
-  header: {
-    height: '3rem',
+  container: {
+    border: '1px solid #ccc',
+  },
+  text: {
+    height: '6rem',
     overflow: 'hidden',
+    padding: '1rem',
   },
   picture: {
     maxHeight: '150px',
     overflow: 'hidden',
+  },
+  subtitle: {
+    fontSize: '0.7rem',
   },
 });
 
